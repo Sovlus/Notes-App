5249df6ea9979e3b56ba2b7e7a395d98222106c5
Sovlus <kubbaz122@gmail.com>
2023-05-20 22:55:44 +0200

    created dodaj notatke

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,15 @@ import { FaTrash } from "react-icons/fa";
 const Main = () => {
   const [notes, setNotes] = useState([]);
   const [inputValue, setImportValue] = useState("");
+
+  const dodajNotatke = () => {
+    if (inputValue.trim() !== '') {
+        const nowa = {
+            id: uuidv4(),
+            content: inputValue;
+        }
+    }
+  }
 };
 
 export default Main;
